14d0a9768650b663d16954765272133d245ed5e1
Satwik Kottur <satwik27@gmail.com>
2018-10-29 13:58:30 -0400

    Fixes run_mnist.sh

--- scripts/run_mnist.sh
+++ scripts/run_mnist.sh
@@ -17,15 +17,10 @@ LRATE=0.0001
 #     --data_root='data/' --model=$MODEL --batch_size=30 --use_refer \
 #     --use_fact --amalgam_text_feats --remove_aux_find
 
-# train the model (example below)
-# python -u exp_mnist/train_eval.py --gpu_id=$GPU_ID --dataset=$DATASET \
-#     --data_root='data/' --model=$MODEL --batch_size=30 --use_refer \
-#     --use_fact --amalgam_text_feats --remove_aux_find
-
 # evaluate a checkpoint (example below)
-#ROOT='checkpoints/'
-ROOT='/coc/pskynet1/skottur3/tfmodel/'
-CHECKPOINT='Wed-24Oct18-16:08:02/model_epoch_156.tmodel'
+# test_split could be 'valid' or 'test'
+ROOT='checkpoints/'
+CHECKPOINT='folder/model_epoch_000.tmodel'
 python -u exp_mnist/eval_sl.py --gpu_id=$GPU_ID \
   --checkpoint=$ROOT$CHECKPOINT --test_split='test'
 
@@ -33,5 +28,3 @@ python -u exp_mnist/eval_sl.py --gpu_id=$GPU_ID \
 #ROOT='/checkpoint/skottur/exp_vd/tfmodel/';
 #PYTHONPATH=. python -u exp_vd/visualize_sl.py -gpuID $GPU_ID \
 #              -checkpoint $ROOT$CHECKPOINT -batchSize 1 -testSplit 'train'
-
-# python util/visualize_mnist.py --batch_path=batch_model.npy
